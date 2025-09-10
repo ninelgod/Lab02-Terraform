a714e2adc57b68830636128a6da1a035bdaf24a7
Emilio Alfaro <137736967+ninelgod@users.noreply.github.com>
2025-09-10 13:33:05 -0500

    feat(postgres.tf): añadir variables de entorno para inicializar la base de datos

--- postgres.tf
+++ postgres.tf
@@ -1,12 +1,12 @@
 resource "docker_container" "postgres" {
   name = "postgres"
   image = "postgres:13.22-bookworm"
-
+  
   networks_advanced {
     name    = docker_network.persistence_net.name
     aliases = ["postgres"]
   }
-
+#en debate si dejamos app_net o no
   networks_advanced {
     name    = docker_network.app_net.name
     aliases = ["postgres"]
@@ -15,4 +15,10 @@ resource "docker_container" "postgres" {
     internal = 5432
     external = 5432
   }
+
+  env = [
+    "POSTGRES_USER=user",
+    "POSTGRES_PASSWORD=12345",
+    "POSTGRES_DB=lab02",
+  ]
 }
