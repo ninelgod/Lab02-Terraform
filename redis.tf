1afdd542f40d144f277eda07490b39ff5608cceb
Emilio Alfaro <137736967+ninelgod@users.noreply.github.com>
2025-09-12 16:50:58 -0500

    refactor: limpiar configuración de redes del contenedor redis

--- redis.tf
+++ redis.tf
@@ -6,11 +6,6 @@ resource "docker_container" "redis" {
     name    = docker_network.persistence_net.name
     aliases = ["redis"]
   }
-
-  networks_advanced {
-    name    = docker_network.app_net.name
-    aliases = ["redis"]
-  }
   ports {
     internal = 6379
     external = var.redis_ports_external[terraform.workspace]
