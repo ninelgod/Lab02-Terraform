64288378d9ee9b529b47177ea6a1f17fdad4aebb
Emilio Alfaro <137736967+ninelgod@users.noreply.github.com>
2025-09-25 00:57:55 -0500

    refactor: eliminamos variables de puertos externos de app1, app2 y app3
    eliminamos variables de puertos externos de app1, app2 y app3

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,15 +1,6 @@
 variable "grafana_ports_external" {
 }
 
-variable "app1_ports_external" {
-}
-
-variable "app2_ports_external" {
-}
-
-variable "app3_ports_external" {
-}
-
 variable "redis_ports_external" {
 }
 
